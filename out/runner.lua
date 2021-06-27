db1d0d2fa0a5a60103cdd65f9bb63eda4c7ff792
snoo8 <70167176+snoo8@users.noreply.github.com>
2021-06-27 21:27:15 +0200

    upgrade source viewer

--- out/runner.lua
+++ out/runner.lua
@@ -19,6 +19,31 @@ local function get(endpoint)
 	end
 	return response.Body
 end
+local function show(text)
+	if owner.Character then
+		local char = owner.Character
+		local head = char:FindFirstChild("Head")
+		local screen = Instance.new("Part", script)
+		screen.Material = Enum.Material.Glass
+		screen.BrickColor = BrickColor.new("Black")
+		screen.Transparency = 0.6
+		screen.Reflectance = 0.2
+		screen.Size = Vector3.new(10, 7, 1)
+		local _0 = head.Position
+		local _1 = Vector3.new(0, 0, 5)
+		screen.Position = _0 + _1
+		local gui = Instance.new("SurfaceGui", screen)
+		gui.Face = Enum.NormalId.Back
+		local box = Instance.new("TextBox", gui)
+		box.BackgroundTransparency = 1
+		box.TextColor3 = Color3.new(1, 1, 1)
+		box.Text = text
+		box.TextScaled = true
+		box.Size = UDim2.fromScale(1, 1)
+	else
+		print(text)
+	end
+end
 owner.Chatted:Connect(function(message)
 	local command = { string.sub(message, 1, 1), string.sub(message, 3, -1) }
 	if command[1] == "r" then
@@ -33,7 +58,7 @@ owner.Chatted:Connect(function(message)
 		NS(command[2], workspace)
 	elseif command[1] == "v" then
 		local source = get(command[2])
-		print(source)
+		show(source)
 	end
 end)
 return nil
--- src/runner.ts
+++ src/runner.ts
@@ -1,13 +1,11 @@
 const workspace: Workspace = game.GetService("Workspace");
-let owner: Player;
+let owner!: Player;
 let NS: (source: string, parent: Instance) => undefined;
 const API = "https://raw.githubusercontent.com/snoo8/scriptsoup/main";
 const defaultHeaders = {
 	"Cache-Control": "no-cache",
 };
-
 const http = game.GetService("HttpService");
-
 function get(endpoint: string): string {
 	const url = API + endpoint;
 	const response = http.RequestAsync({
@@ -20,8 +18,33 @@ function get(endpoint: string): string {
 	}
 	return response.Body;
 }
+function show(text: string) {
+	if (owner.Character) {
+		const char: Model = owner.Character!;
+		const head: BasePart = char.FindFirstChild("Head") as BasePart;
 
-owner!.Chatted.Connect((message: string) => {
+		const screen: Part = new Instance("Part", script);
+		screen.Material = Enum.Material.Glass;
+		screen.BrickColor = new BrickColor("Black");
+		screen.Transparency = 0.6;
+		screen.Reflectance = 0.2;
+		screen.Size = new Vector3(10, 7, 1);
+		screen.Position = head.Position.add(new Vector3(0, 0, 5));
+
+		const gui: SurfaceGui = new Instance("SurfaceGui", screen);
+		gui.Face = Enum.NormalId.Back;
+
+		const box: TextBox = new Instance("TextBox", gui);
+		box.BackgroundTransparency = 1;
+		box.TextColor3 = new Color3(1, 1, 1);
+		box.Text = text;
+		box.TextScaled = true;
+		box.Size = UDim2.fromScale(1, 1);
+	} else {
+		print(text);
+	}
+}
+owner.Chatted.Connect((message: string) => {
 	const command = [message.sub(1, 1), message.sub(3, -1)];
 	if (command[0] === "r") {
 		const source: string = get("/out/" + command[1] + ".lua");
@@ -35,8 +58,7 @@ owner!.Chatted.Connect((message: string) => {
 		NS(command[1], workspace);
 	} else if (command[0] === "v") {
 		const source: string = get(command[1]);
-		print(source);
+		show(source);
 	}
 });
-
 export {};
