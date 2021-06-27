61e1468871e4db83e8545d72f92992cd9bf74ca8
snoo8 <70167176+snoo8@users.noreply.github.com>
2021-06-27 14:37:44 +0200

    fix runner

--- out/runner.lua
+++ out/runner.lua
@@ -1,11 +1,21 @@
 -- Compiled with roblox-ts v1.1.1
 local NS
 local owner
+local http = game:GetService("HttpService")
 owner.Chatted:Connect(function(message)
 	local command = string.split(message, "'")
 	if command[1] == "r" then
+		local source = http:GetAsync("https://raw.githubusercontent.com/snoo8/scriptsoup/main/out/" .. command[2] .. ".lua")
+		if source ~= "" and source then
+			NS(source, script)
+		else
+			warn("Invalid script name!")
+		end
 	elseif command[1] == "c" then
 		NS(command[2], script)
+	elseif command[1] == "v" then
+		local source = http:GetAsync("https://raw.githubusercontent.com/snoo8/scriptsoup/main/" .. command[2])
+		print(source)
 	end
 end)
 return nil
--- src/runner.ts
+++ src/runner.ts
@@ -1,14 +1,21 @@
-import { HttpService } from "@rbxts/services";
-
 let NS: (source: string, parent: Instance) => void;
-let owner: Player;
+let owner!: Player;
+const http = game.GetService("HttpService");
 
-owner!.Chatted.Connect((message: string) => {
+owner.Chatted.Connect((message: string) => {
 	const command = message.split("'");
 	if (command[0] === "r") {
-
+		const source: string = http.GetAsync("https://raw.githubusercontent.com/snoo8/scriptsoup/main/out/" + command[1] + ".lua");
+		if (source) {
+			NS(source, script);
+		} else {
+			warn("Invalid script name!");
+		}
 	} else if (command[0] === "c") {
-		NS!(command[1], script);
+		NS(command[1], script);
+	} else if (command[0] === "v") {
+		const source: string = http.GetAsync("https://raw.githubusercontent.com/snoo8/scriptsoup/main/" + command[1]);
+		print(source);
 	}
 })
 
