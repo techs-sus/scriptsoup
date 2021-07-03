ef162d505cd088a647757cd3bab933048f78d6a4
snoo8 <70167176+snoo8@users.noreply.github.com>
2021-07-03 17:13:25 +0200

    add tormux run

--- out/tormux.lua
+++ out/tormux.lua
@@ -7,18 +7,12 @@ local _1 = CFrame.new(0, 0, -5)
 screen.CFrame = _0 * _1
 local UI = screen:FindFirstChild("SurfaceGui"):FindFirstChild("Frame")
 local out = UI:FindFirstChild("output")
-local template = out:FindFirstChild("1")
-out:FindFirstChild("0"):Destroy()
-template.Parent = nil
 local remote = Instance.new("RemoteEvent", owner:FindFirstChild("PlayerGui"))
-local remoteBack = Instance.new("RemoteEvent", remote)
-remoteBack.Name = "remback"
 local ref = Instance.new("ObjectValue", remote)
 ref.Name = "ref"
 ref.Value = screen
 NLS([[
 		local rem = script.Parent
-		local remback = rem:WaitForChild("remback")
 		local ref = rem:WaitForChild("ref")
 		local screen = ref.Value
 		script.Parent = rem.Parent
@@ -33,9 +27,14 @@ NLS([[
 		local box = UI:WaitForChild("input")
 		local go = box:WaitForChild("go")
 		local out = UI:WaitForChild("output")
+		print('out')
+		table.foreach(out:GetChildren(), print)
 		local log = out:WaitForChild("help")
+		print('hel')
 		out:WaitForChild("welcome"):Destroy()
+		print('des')
 		log.Parent = nil
+		print('go')
 
 		go.MouseButton1Click:Connect(function()
 			local text = box.Text
@@ -43,18 +42,35 @@ NLS([[
 			box.Text = ""
 			rem:FireServer("exec", text)
 		end)
-		remback.OnClientEvent:Connect(function(text)
-			local new = log:Clone()
-			new.Name = os.clock()
-			new.Text = text
-			new.Parent = out
-		end)
+		rem:FireServer("init")
 	]], remote)
-remote.OnServerEvent:Connect(function(player, mode, meta)
-	print(player, mode, meta)
-	remoteBack:FireClient(owner, "~ " .. tostring(meta))
+local template = out:FindFirstChild("help")
+local function log(text)
 	local box = template:Clone()
 	box.Name = tostring(os.clock())
-	box.Text = "~ " .. tostring(meta)
+	box.Text = text
+	box.Parent = out
+end
+remote.OnServerEvent:Connect(function(player, mode, meta)
+	print(player, mode, meta)
+	if mode == "exec" then
+		log("~ " .. tostring(meta))
+		local run = loadstring(meta)
+		local env = getfenv(0)
+		env.terminal = {
+			log = log,
+			help = function()
+				log("terminal.log ( text: string ) -> void / log to output")
+				log("terminal.help () -> void / show help")
+			end,
+		}
+		setfenv(run, env)
+		run()
+	elseif mode == "init" then
+		out:FindFirstChild("welcome"):Destroy()
+		template.Parent = nil
+		log("Welcome to Tormux")
+		log("Enter terminal.help() for help")
+	end
 end)
 return nil
--- src/tormux.ts
+++ src/tormux.ts
@@ -1,6 +1,7 @@
 declare const owner: Player;
 declare const loadstring: (source: string) => Function;
 declare const NLS: (source: string, parent: Instance) => undefined;
+declare const setfenv: (func: Function, env: unknown) => undefined;
 
 const http = game.GetService("HttpService");
 const screen = loadstring(
@@ -10,20 +11,14 @@ screen.Parent = script;
 screen.CFrame = (owner.Character!.FindFirstChild("Head") as Part).CFrame.mul(new CFrame(0, 0, -5));
 const UI = screen.FindFirstChild("SurfaceGui")!.FindFirstChild("Frame")!;
 const out = UI.FindFirstChild("output")!;
-const template = out.FindFirstChild("1")!;
-out.FindFirstChild("0")!.Destroy();
-template.Parent = undefined;
 
 const remote = new Instance("RemoteEvent", owner.FindFirstChild("PlayerGui")!);
-const remoteBack = new Instance("RemoteEvent", remote);
-remoteBack.Name = "remback";
 const ref = new Instance("ObjectValue", remote);
 ref.Name = "ref";
 ref.Value = screen;
 NLS(
 	`
 		local rem = script.Parent
-		local remback = rem:WaitForChild("remback")
 		local ref = rem:WaitForChild("ref")
 		local screen = ref.Value
 		script.Parent = rem.Parent
@@ -38,9 +33,14 @@ NLS(
 		local box = UI:WaitForChild("input")
 		local go = box:WaitForChild("go")
 		local out = UI:WaitForChild("output")
+		print('out')
+		table.foreach(out:GetChildren(), print)
 		local log = out:WaitForChild("help")
+		print('hel')
 		out:WaitForChild("welcome"):Destroy()
+		print('des')
 		log.Parent = nil
+		print('go')
 
 		go.MouseButton1Click:Connect(function()
 			local text = box.Text
@@ -48,21 +48,38 @@ NLS(
 			box.Text = ""
 			rem:FireServer("exec", text)
 		end)
-		remback.OnClientEvent:Connect(function(text)
-			local new = log:Clone()
-			new.Name = os.clock()
-			new.Text = text
-			new.Parent = out
-		end)
+		rem:FireServer("init")
 	`,
 	remote,
 );
-remote.OnServerEvent.Connect((player: Player, mode: unknown, meta: unknown) => {
-	print(player, mode, meta);
-	remoteBack.FireClient(owner, "~ " + meta);
+const template = out.FindFirstChild("help")!;
+function log(text: string) {
 	const box = template.Clone() as TextBox;
 	box.Name = tostring(os.clock());
-	box.Text = "~ " + meta;
+	box.Text = text;
+	box.Parent = out;
+}
+remote.OnServerEvent.Connect((player: Player, mode: unknown, meta: unknown) => {
+	print(player, mode, meta);
+	if (mode === "exec") {
+		log("~ " + meta);
+		const run = loadstring(meta as string);
+		const env = getfenv(0) as { [type: string]: unknown };
+		env.terminal = {
+			log: log,
+			help: () => {
+				log("terminal.log ( text: string ) -> void / log to output");
+				log("terminal.help () -> void / show help");
+			},
+		};
+		setfenv(run, env);
+		run();
+	} else if (mode === "init") {
+		out.FindFirstChild("welcome")!.Destroy();
+		template.Parent = undefined;
+		log("Welcome to Tormux");
+		log("Enter terminal.help() for help");
+	}
 });
 
 export {};
