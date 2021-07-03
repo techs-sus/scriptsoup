7a135da3047c5f9f8ff1ff138d8cfa7bca32a35d
snoo8 <70167176+snoo8@users.noreply.github.com>
2021-07-03 20:45:02 +0200

    add dynamic widgets to tormux

--- out/tormux.lua
+++ out/tormux.lua
@@ -7,6 +7,7 @@ local _1 = CFrame.new(0, 0, -5)
 screen.CFrame = _0 * _1
 local UI = screen:FindFirstChild("SurfaceGui"):FindFirstChild("Frame")
 local out = UI:FindFirstChild("output")
+local widgets = UI:FindFirstChild("widgets")
 local remote = Instance.new("RemoteEvent", owner:FindFirstChild("PlayerGui"))
 local ref = Instance.new("ObjectValue", remote)
 ref.Name = "ref"
@@ -44,33 +45,73 @@ NLS([[
 		end)
 		rem:FireServer("init")
 	]], remote)
-local template = out:FindFirstChild("help")
+local logTemplate = out:FindFirstChild("help")
+local widgetTemplate = widgets:FindFirstChild("1")
+local availableWidgets = {
+	time = function()
+		local widget = widgetTemplate:Clone()
+		coroutine.wrap(function()
+			while true do
+				widget.Text = os.date("%H:%M")
+				wait(60)
+			end
+		end)()
+		return widget
+	end,
+	quote = function()
+		local widget = widgetTemplate:Clone()
+		coroutine.wrap(function()
+			while true do
+				widget.Text = http:GetAsync("http://api.quotable.io/random")
+				wait(35)
+			end
+		end)()
+		return widget
+	end,
+}
 local function log(text)
-	local box = template:Clone()
+	local box = logTemplate:Clone()
 	box.Name = tostring(os.clock())
 	box.Text = text
 	box.Parent = out
 end
+local env = getfenv(0)
+local terminalLib = {
+	log = log,
+	help = function()
+		log("terminal.log ( text: string ) -> void / log to output")
+		log("terminal.help () -> void / show help")
+		log("terminal.setWidget ( num: string, widget: Widget ) -> void / set widget")
+		log("terminal.widgets -> Widget[] / list of widgets")
+	end,
+	widgets = availableWidgets,
+	setWidget = function(num, widget)
+		local _2 = widgets:FindFirstChild(num)
+		if _2 ~= nil then
+			_2:Destroy()
+		end
+		local newWidget = widget()
+		newWidget.Name = num
+		newWidget.Parent = widgets
+	end,
+}
+env.terminal = terminalLib
 remote.OnServerEvent:Connect(function(player, mode, meta)
 	print(player, mode, meta)
 	if mode == "exec" then
 		log("~ " .. tostring(meta))
 		local run = loadstring(meta)
-		local env = getfenv(0)
-		env.terminal = {
-			log = log,
-			help = function()
-				log("terminal.log ( text: string ) -> void / log to output")
-				log("terminal.help () -> void / show help")
-			end,
-		}
 		setfenv(run, env)
 		run()
 	elseif mode == "init" then
 		out:FindFirstChild("welcome"):Destroy()
-		template.Parent = nil
+		widgets:FindFirstChild("2"):Destroy()
+		logTemplate.Parent = nil
+		widgetTemplate.Parent = nil
 		log("Welcome to Tormux")
 		log("Enter terminal.help() for help")
+		terminalLib.setWidget("1", availableWidgets.quote)
+		terminalLib.setWidget("2", availableWidgets.time)
 	end
 end)
 return nil
--- src/tormux.ts
+++ src/tormux.ts
@@ -3,6 +3,8 @@ declare const loadstring: (source: string) => Function;
 declare const NLS: (source: string, parent: Instance) => undefined;
 declare const setfenv: (func: Function, env: unknown) => undefined;
 
+type widgetify = () => TextBox;
+
 const http = game.GetService("HttpService");
 const screen = loadstring(
 	http.GetAsync("https://raw.githubusercontent.com/snoo8/scriptsoup/main/models/tormuxUI.lua"),
@@ -11,6 +13,7 @@ screen.Parent = script;
 screen.CFrame = (owner.Character!.FindFirstChild("Head") as Part).CFrame.mul(new CFrame(0, 0, -5));
 const UI = screen.FindFirstChild("SurfaceGui")!.FindFirstChild("Frame")!;
 const out = UI.FindFirstChild("output")!;
+const widgets = UI.FindFirstChild("widgets")!;
 
 const remote = new Instance("RemoteEvent", owner.FindFirstChild("PlayerGui")!);
 const ref = new Instance("ObjectValue", remote);
@@ -52,33 +55,70 @@ NLS(
 	`,
 	remote,
 );
-const template = out.FindFirstChild("help")!;
+const logTemplate = out.FindFirstChild("help")!;
+const widgetTemplate = widgets.FindFirstChild("1")!;
+const availableWidgets = {
+	time: () => {
+		const widget = widgetTemplate.Clone() as TextBox;
+		coroutine.wrap(() => {
+			while (true) {
+				widget.Text = os.date("%H:%M");
+				wait(60);
+			}
+		})();
+		return widget;
+	},
+	quote: () => {
+		const widget = widgetTemplate.Clone() as TextBox;
+		coroutine.wrap(() => {
+			while (true) {
+				widget.Text = http.GetAsync("http://api.quotable.io/random");
+				wait(35);
+			}
+		})();
+		return widget;
+	},
+};
 function log(text: string) {
-	const box = template.Clone() as TextBox;
+	const box = logTemplate.Clone() as TextBox;
 	box.Name = tostring(os.clock());
 	box.Text = text;
 	box.Parent = out;
 }
+const env = getfenv(0) as { [type: string]: unknown };
+const terminalLib = {
+	log: log,
+	help: () => {
+		log("terminal.log ( text: string ) -> void / log to output");
+		log("terminal.help () -> void / show help");
+		log("terminal.setWidget ( num: string, widget: Widget ) -> void / set widget");
+		log("terminal.widgets -> Widget[] / list of widgets");
+	},
+	widgets: availableWidgets,
+	setWidget: (num: string, widget: widgetify) => {
+		widgets.FindFirstChild(num)?.Destroy();
+		const newWidget = widget();
+		newWidget.Name = num;
+		newWidget.Parent = widgets;
+	},
+};
+env.terminal = terminalLib;
 remote.OnServerEvent.Connect((player: Player, mode: unknown, meta: unknown) => {
 	print(player, mode, meta);
 	if (mode === "exec") {
 		log("~ " + meta);
 		const run = loadstring(meta as string);
-		const env = getfenv(0) as { [type: string]: unknown };
-		env.terminal = {
-			log: log,
-			help: () => {
-				log("terminal.log ( text: string ) -> void / log to output");
-				log("terminal.help () -> void / show help");
-			},
-		};
 		setfenv(run, env);
 		run();
 	} else if (mode === "init") {
 		out.FindFirstChild("welcome")!.Destroy();
-		template.Parent = undefined;
+		widgets.FindFirstChild("2")!.Destroy();
+		logTemplate.Parent = undefined;
+		widgetTemplate.Parent = undefined;
 		log("Welcome to Tormux");
 		log("Enter terminal.help() for help");
+		terminalLib.setWidget("1", availableWidgets.quote);
+		terminalLib.setWidget("2", availableWidgets.time);
 	}
 });
 
